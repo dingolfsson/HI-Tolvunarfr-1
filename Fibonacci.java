d0247689b6b58cb58f912b141c3c84797358e259
dingolfsson <dai5@hi.is>
2017-10-27 11:04:06 +0000

    Tribonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,13 +1,19 @@
 public class Fibonacci {
-    public static long fibonacci(int n) {
-        if (n <= 1) return n;
-        else return fibonacci(n-1) + fibonacci(n-2) + fibonacci(n-3);
+    public static int triboRec(int n) {
+        if (n == 1) 
+            return 0;
+        else if (n == 2) 
+            return 0;
+        else if (n == 3) 
+            return 1;
+        else return (triboRec(n-1)+triboRec(n-2)+triboRec(n-3));
     }
 
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        for (int i = 1; i <= n; i++)
-            StdOut.println(i + ": " + fibonacci(i));
+        for (int i = 1; i <= n; i++) {
+            System.out.print(triboRec(i) + " ");
+        }
+        System.out.println("");
     }
-
 }
